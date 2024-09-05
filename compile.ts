d789d79aa801c8a46fe6c2b102dfd9ceb0c6af51
Santi <santi.gamo@gmail.com>
2024-09-05 12:14:26 +0200

    update

--- compile.ts
+++ compile.ts
@@ -3,7 +3,7 @@ import sindri from 'sindri';
 async function main() {
   try {
     console.log('Compiling circuit...');
-    // Compile the circuit in the `is-equal` directory.
+    // Compile the circuit in the `packages/circuit` directory.
     const circuit = await sindri.createCircuit('packages/circuit');
 
     // Log out the circuit object as JSON.
