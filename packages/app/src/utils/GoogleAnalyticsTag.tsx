f4a461341631cf90621f2c3024c4745dea458dfc
Jofra Montesdeoca <jmontesdeocanuez21@gmail.com>
2025-05-06 11:03:37 +0100

    refactor: abstract GA ID to env variable

--- packages/app/src/utils/GoogleAnalyticsTag.tsx
+++ packages/app/src/utils/GoogleAnalyticsTag.tsx
@@ -1,17 +1,23 @@
 import Script from 'next/script';
 
+const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+
 const GoogleAnalyticsTag = () => {
+  if (!GOOGLE_ANALYTICS_ID) {
+    return null;
+  }
+
   return (
     <>
       <Script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-GCLRBTPM0K"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
       ></Script>
       <Script id="gtag-script">
         {`window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments)}
         gtag('js', new Date());
-        gtag('config', 'G-GCLRBTPM0K');`}
+        gtag('config', '${GOOGLE_ANALYTICS_ID}');`}
       </Script>
     </>
   );
